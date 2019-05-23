ce43636aa0f121aab8a5a29fc2e8bf04f81e9c46
Derek Brimley <derek.brimley@forcepoint.com>
2019-05-23 15:44:33 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 // const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-
 const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, () => {
+const server = require('http').createServer(app)
+server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
 const io = require('socket.io')(server);
